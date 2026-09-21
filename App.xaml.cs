756a0a678dfc6cdee6623b3a93f6415116f56b0a
leslie <ortegaaleslie73@gmail.com>
2026-09-20 18:38:03 -0600

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Threading;
+using System.Windows;
+
+namespace SushiGo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("es-MX");
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("es-MX");
+
+            InitializeComponent();
+        }
+    }
+}
